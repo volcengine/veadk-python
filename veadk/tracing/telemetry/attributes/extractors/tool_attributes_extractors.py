cf26b141cecf1500cdb055e1761f1bbefa89bdb2
hanzhi <53893931+zakahan@users.noreply.github.com>
2025-08-26 20:57:38 +0800

    fix: tool_call bug (#107)
    * fix: tool_call dump bug
    * fix: tool_call dump ascii bug

--- veadk/tracing/telemetry/attributes/extractors/tool_attributes_extractors.py
+++ veadk/tracing/telemetry/attributes/extractors/tool_attributes_extractors.py
@@ -44,7 +44,9 @@ def tool_cozeloop_input(params: ToolAttributesParams) -> ExtractorResponse:
         "description": params.tool.description,
         "parameters": params.args,
     }
-    return ExtractorResponse(content=json.dumps(tool_input) or "<unknown_tool_input>")
+    return ExtractorResponse(
+        content=json.dumps(tool_input, ensure_ascii=False) or "<unknown_tool_input>"
+    )
 
 
 def tool_gen_ai_tool_name(params: ToolAttributesParams) -> ExtractorResponse:
@@ -52,13 +54,17 @@ def tool_gen_ai_tool_name(params: ToolAttributesParams) -> ExtractorResponse:
 
 
 def tool_cozeloop_output(params: ToolAttributesParams) -> ExtractorResponse:
-    function_response = params.function_response_event.get_function_responses()[0]
+    function_response = params.function_response_event.get_function_responses()[
+        0
+    ].model_dump()
     tool_output = {
-        "id": function_response.id,
-        "name": function_response.name,
-        "response": function_response.response,
+        "id": function_response["id"],
+        "name": function_response["name"],
+        "response": function_response["response"],
     }
-    return ExtractorResponse(content=json.dumps(tool_output) or "<unknown_tool_output>")
+    return ExtractorResponse(
+        content=json.dumps(tool_output, ensure_ascii=False) or "<unknown_tool_output>"
+    )
 
 
 TOOL_ATTRIBUTES = {
