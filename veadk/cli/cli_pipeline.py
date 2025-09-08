5aa4a4cdc5377d82b3b8f2d23a59012eec55a6e4
fangyaozheng@bytedance.com <fangyaozheng@bytedance.com>
2025-09-08 18:40:45 +0800

    supply docs and fix image deploy issues

--- veadk/cli/cli_pipeline.py
+++ veadk/cli/cli_pipeline.py
@@ -15,16 +15,21 @@
 import warnings
 
 import click
-from veadk.version import VERSION
+
 from veadk.config import getenv
-from veadk.integrations.ve_code_pipeline.ve_code_pipeline import VeCodePipeline
-from veadk.integrations.ve_faas.ve_faas import VeFaaS
-from veadk.integrations.ve_cr.ve_cr import VeCR
 from veadk.consts import (
     DEFAULT_CR_INSTANCE_NAME,
     DEFAULT_CR_NAMESPACE_NAME,
     DEFAULT_CR_REPO_NAME,
 )
+from veadk.integrations.ve_code_pipeline.ve_code_pipeline import VeCodePipeline
+from veadk.integrations.ve_cr.ve_cr import VeCR
+from veadk.integrations.ve_faas.ve_faas import VeFaaS
+from veadk.utils.logger import get_logger
+from veadk.version import VERSION
+
+logger = get_logger(__name__)
+
 
 warnings.filterwarnings(
     "ignore", category=UserWarning, module="pydantic._internal._fields"
@@ -65,9 +70,9 @@ def _create_cr(volcengine_settings: dict[str, str], cr_settings: dict[str, str])
 
 @click.command()
 @click.option(
-    "--base-image-tag",
-    required=True,
-    help=f"Base VeADK image tag can be 'preview', 'latest', or a VeADK version (e.g., {VERSION})",
+    "--veadk-version",
+    default=VERSION,
+    help=f"Base VeADK image tag can be 'preview', 'latest', or a specific VeADK version (e.g., {VERSION})",
 )
 @click.option(
     "--github-url",
@@ -76,8 +81,8 @@ def _create_cr(volcengine_settings: dict[str, str], cr_settings: dict[str, str])
 )
 @click.option(
     "--github-branch",
-    default="main",
-    help="The github branch of your project, default is main",
+    required=True,
+    help="The github branch of your project",
 )
 @click.option(
     "--github-token",
@@ -85,58 +90,52 @@ def _create_cr(volcengine_settings: dict[str, str], cr_settings: dict[str, str])
     help="The github token to manage your project",
 )
 @click.option(
-    "--access-key",
-    default=getenv("VOLCENGINE_ACCESS_KEY"),
+    "--volcengine-access-key",
+    default=None,
     help="Volcengine access key, if not set, will use the value of environment variable VOLCENGINE_ACCESS_KEY",
 )
 @click.option(
-    "--secret-key",
-    default=getenv("VOLCENGINE_SECRET_KEY"),
+    "--volcengine-secret-key",
+    default=None,
     help="Volcengine secret key, if not set, will use the value of environment variable VOLCENGINE_SECRET_KEY",
 )
 @click.option(
     "--region",
     default="cn-beijing",
-    help="Volcengine region, default is cn-beijing",
+    help="Region for Volcengine VeFaaS, CR, and Pipeline. Default is cn-beijing",
 )
 @click.option(
     "--cr-instance-name",
     default=DEFAULT_CR_INSTANCE_NAME,
-    help="Container Registry instance name, default is veadk-user-instance",
+    help="Volcengine Container Registry instance name, default is veadk-user-instance",
 )
 @click.option(
     "--cr-namespace-name",
     default=DEFAULT_CR_NAMESPACE_NAME,
-    help="Container Registry namespace name, default is veadk-user-namespace",
+    help="Volcengine Container Registry namespace name, default is veadk-user-namespace",
 )
 @click.option(
     "--cr-repo-name",
     default=DEFAULT_CR_REPO_NAME,
-    help="Container Registry repo name, default is veadk-user-repo",
+    help="Volcengine Container Registry repo name, default is veadk-user-repo",
 )
 @click.option(
-    "--cr-region",
-    default="cn-beijing",
-    help="Container Registry region, default is cn-beijing",
-)
-@click.option(
-    "--function-id",
+    "--vefaas-function-id",
     default=None,
     help="Volcengine FaaS function ID, if not set, a new function will be created automatically",
 )
 def pipeline(
-    base_image_tag: str,
+    veadk_version: str,
     github_url: str,
     github_branch: str,
     github_token: str,
-    access_key: str,
-    secret_key: str,
+    volcengine_access_key: str,
+    volcengine_secret_key: str,
     region: str,
     cr_instance_name: str,
     cr_namespace_name: str,
     cr_repo_name: str,
-    cr_region: str,
-    function_id: str,
+    vefaas_function_id: str,
 ) -> None:
     """Integrate a veadk project to volcengine pipeline for CI/CD"""
 
@@ -144,36 +143,36 @@ def pipeline(
         "Welcome use VeADK to integrate your project to volcengine pipeline for CI/CD."
     )
 
+    if not volcengine_access_key:
+        volcengine_access_key = getenv("VOLCENGINE_ACCESS_KEY")
+    if not volcengine_secret_key:
+        volcengine_secret_key = getenv("VOLCENGINE_SECRET_KEY")
+
     volcengine_settings = {
-        "volcengine_access_key": access_key,
-        "volcengine_secret_key": secret_key,
+        "volcengine_access_key": volcengine_access_key,
+        "volcengine_secret_key": volcengine_secret_key,
         "volcengine_region": region,
     }
 
     cr_settings = {
-        "cr_domain": f"{cr_instance_name}-{cr_region}.cr.volces.com",
+        "cr_domain": f"{cr_instance_name}-{region}.cr.volces.com",
         "cr_instance_name": cr_instance_name,
         "cr_namespace_name": cr_namespace_name,
         "cr_repo_name": cr_repo_name,
-        "cr_region": cr_region,
+        "cr_region": region,
     }
+
+    if not vefaas_function_id:
+        click.echo(
+            "No Function ID specified. VeADK will create one automatically. Please specify a function name:"
+        )
+        function_name = click.prompt(
+            "Function name", default="veadk-image-function", show_default=False
+        )
 
     _create_cr(volcengine_settings, cr_settings)
 
-    click.echo("Using the following CR configuration:")
-    click.echo(f"Container Registry domain: {cr_settings['cr_domain']}")
-    click.echo(f"Container Registry namespace name: {cr_settings['cr_namespace_name']}")
-    click.echo(f"Container Registry region: {cr_settings['cr_region']}")
-    click.echo(f"Container Registry instance name: {cr_settings['cr_instance_name']}")
-    click.echo(f"Container Registry repo name: {cr_settings['cr_repo_name']}")
-
-    if not function_id:
-        click.echo(
-            "No Function ID specified. The system will create one automatically. Please specify a function name:"
-        )
-        function_name = click.prompt(
-            "Function name", default="veadk-function", show_default=False
-        )
+    if not vefaas_function_id:
         vefaas_client = VeFaaS(
             access_key=volcengine_settings["volcengine_access_key"],
             secret_key=volcengine_settings["volcengine_secret_key"],
@@ -184,15 +183,29 @@ def pipeline(
             image="veadk-cn-beijing.cr.volces.com/veadk/simple-fastapi:0.1",
             registry_name=cr_settings["cr_instance_name"],
         )
-        click.echo(f"Created function {function_name} with ID: {function_id}")
+        logger.debug(f"Created function {function_name} with ID: {function_id}")
 
     client = VeCodePipeline(
         volcengine_access_key=volcengine_settings["volcengine_access_key"],
         volcengine_secret_key=volcengine_settings["volcengine_secret_key"],
         region=volcengine_settings["volcengine_region"],
     )
+
+    click.echo("=====================================================")
+    click.echo("Using the following configuration to create pipeline:")
+    click.echo(f"Use VeADK version: {veadk_version}")
+    click.echo(f"Github url: {github_url}")
+    click.echo(f"Github branch: {github_branch}")
+    click.echo(f"VeFaaS function name: {function_name}")
+    click.echo(f"VeFaaS function ID: {function_id}")
+    click.echo(f"Container Registry domain: {cr_settings['cr_domain']}")
+    click.echo(f"Container Registry namespace name: {cr_settings['cr_namespace_name']}")
+    click.echo(f"Container Registry region: {region}")
+    click.echo(f"Container Registry instance name: {cr_settings['cr_instance_name']}")
+    click.echo(f"Container Registry repo name: {cr_settings['cr_repo_name']}")
+
     client.deploy(
-        base_image_tag=base_image_tag,
+        base_image_tag=veadk_version,
         github_url=github_url,
         github_branch=github_branch,
         github_token=github_token,
