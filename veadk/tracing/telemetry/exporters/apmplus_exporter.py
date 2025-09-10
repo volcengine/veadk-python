4887e14d2f8bd82eaaacb08c8dd73db18415c2ae
fangyaozheng@bytedance.com <fangyaozheng@bytedance.com>
2025-09-10 15:52:18 +0800

    fix import

--- veadk/tracing/telemetry/exporters/apmplus_exporter.py
+++ veadk/tracing/telemetry/exporters/apmplus_exporter.py
@@ -12,9 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from dataclasses import dataclass
 from typing import Any
 
-from attr import dataclass
 from google.adk.models.llm_request import LlmRequest
 from google.adk.models.llm_response import LlmResponse
 from opentelemetry import metrics
