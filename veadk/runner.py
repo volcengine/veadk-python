5452f8e8c98b8dfbe56afcc12de09c7eaa563374
yaozheng-fang <fangyaozheng@bytedance.com>
2025-10-17 10:32:45 +0800

    chore(tools): support artifact in image generation tools (#226)

--- veadk/runner.py
+++ veadk/runner.py
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 import functools
+import os
 from types import MethodType
 from typing import Union
 
