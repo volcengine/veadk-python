8e6fa157032e2dda6d1c29d34c04b8f9ed8dba7e
yaozheng-fang <fangyaozheng@bytedance.com>
2025-11-05 10:26:01 +0800

    fix(tools): fix vesearch.py return env name (#283)
    * Update vesearch.py
    
    * Update vesearch.py (#284)

--- veadk/tools/builtin_tools/vesearch.py
+++ veadk/tools/builtin_tools/vesearch.py
@@ -30,9 +30,9 @@ def vesearch(query: str) -> str:
     bot_id = str(getenv("TOOL_VESEARCH_ENDPOINT"))
 
     if api_key == "":
-        return "Invoke `vesearch` failed. Please set VESEARCH_API_KEY as your environment variable."
+        return "Invoke `vesearch` failed. Please set TOOL_VESEARCH_API_KEY as your environment variable."
     if bot_id == "":
-        return "Invoke `vesearch` failed. Please set VESEARCH_BOT_ID as your environment variable."
+        return "Invoke `vesearch` failed. Please set TOOL_VESEARCH_ENDPOINT as your environment variable."
 
     URL = "https://open.feedcoopapi.com/agent_api/agent/chat/completion"
     headers = {"Authorization": f"Bearer {api_key}", "Content-Type": "application/json"}
