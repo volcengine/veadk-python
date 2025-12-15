7bbf2366e9cfe6780634c4b2ecc2c35686d80d44
wangyue.demon <wangyue.demon@bytedance.com>
2025-12-15 20:26:18 +0800

    fix ut: TestRealtimeModelConfig Clear any cached properties before each test

--- tests/config/test_model_config.py
+++ tests/config/test_model_config.py
@@ -1,14 +1,23 @@
+# Copyright (c) 2025 Beijing Volcano Engine Technology Co., Ltd. and/or its affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 import os
 from unittest import TestCase, mock
 from veadk.configs.model_configs import RealtimeModelConfig
 
 
 class TestRealtimeModelConfig(TestCase):
-    def setUp(self):
-        # Clear any cached properties before each test
-        if hasattr(RealtimeModelConfig, "api_key"):
-            del RealtimeModelConfig.api_key
-
     def test_default_values(self):
         """Test that default values are set correctly"""
         config = RealtimeModelConfig()
