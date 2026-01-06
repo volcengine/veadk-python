6f12fb3a4f8f96c57413a20f9da72531133ad9b8
warm-wm <wangmeng.823@bytedance.com>
2026-01-06 22:35:13 +0800

    feat: add playwright tool for browser use in local environment; agent in sandbox can use it directly

---
+++ veadk/tools/builtin_tools/playwright.py
@@ -0,0 +1,33 @@
+from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset, StdioConnectionParams, StdioServerParameters
+import subprocess
+from veadk.utils.logger import get_logger
+
+logger = get_logger(__name__)
+
+def check_env():
+    try:
+        result = subprocess.run(
+            ["npx", "-v"], capture_output=True, text=True, check=True
+        )
+        version = result.stdout.strip()
+        logger.info(f"Check `npx` command done, version: {version}")
+    except Exception as e:
+        raise Exception(
+            "Check `npx` command failed. Please install `npx` command manually."
+        ) from e
+
+check_env()
+
+playwright_tools = MCPToolset(
+    connection_params=StdioConnectionParams(
+        server_params=StdioServerParameters(
+            command='npx',
+            args=[
+                "-y",
+                "@playwright/mcp@latest",
+            ],
+        ),
+        timeout=30
+    ),
+    # tool_filter=['browser_navigate', 'browser_screenshot', 'browser_fill', 'browser_click']
+)
