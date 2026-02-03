3d8c814f4b8d47a8c7c442f4550b4509609a9c50
hanzhi <53893931+zakahan@users.noreply.github.com>
2026-02-03 17:21:08 +0800

    fix: adjustment of viking dependencies (#489)

--- veadk/knowledgebase/backends/utils.py
+++ veadk/knowledgebase/backends/utils.py
@@ -12,9 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import json
 from pathlib import Path
-from typing import Literal
 
 from llama_index.core.node_parser import (
     CodeSplitter,
@@ -22,9 +20,6 @@ from llama_index.core.node_parser import (
     MarkdownNodeParser,
     SentenceSplitter,
 )
-from volcengine.auth.SignerV4 import SignerV4
-from volcengine.base.Request import Request
-from volcengine.Credentials import Credentials
 
 
 def get_llama_index_splitter(
@@ -40,53 +35,3 @@ def get_llama_index_splitter(
         return HTMLNodeParser()
     else:
         return SentenceSplitter(chunk_size=512, chunk_overlap=50)
-
-
-def build_vikingdb_knowledgebase_request(
-    path: str,
-    volcengine_access_key: str,
-    volcengine_secret_key: str,
-    session_token: str = "",
-    method: Literal["GET", "POST", "PUT", "DELETE"] = "POST",
-    region: str = "cn-beijing",
-    params=None,
-    data=None,
-    doseq=0,
-) -> Request:
-    if params:
-        for key in params:
-            if (
-                type(params[key]) is int
-                or type(params[key]) is float
-                or type(params[key]) is bool
-            ):
-                params[key] = str(params[key])
-            elif type(params[key]) is list:
-                if not doseq:
-                    params[key] = ",".join(params[key])
-
-    r = Request()
-    r.set_shema("https")
-    r.set_method(method)
-    r.set_connection_timeout(10)
-    r.set_socket_timeout(10)
-
-    mheaders = {
-        "Accept": "application/json",
-        "Content-Type": "application/json",
-    }
-    r.set_headers(mheaders)
-
-    if params:
-        r.set_query(params)
-
-    r.set_path(path)
-
-    if data is not None:
-        r.set_body(json.dumps(data))
-
-    credentials = Credentials(
-        volcengine_access_key, volcengine_secret_key, "air", region, session_token
-    )
-    SignerV4.sign(r, credentials)
-    return r
