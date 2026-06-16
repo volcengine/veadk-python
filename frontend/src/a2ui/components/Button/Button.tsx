e0870b0647b4c1049458ce98bada670579f9e1a0
zhengchuyi <zhengchuyi@bytedance.com>
2026-06-16 16:29:42 +0800

    feat: a2ui example for flight info

--- frontend/src/a2ui/components/Button/Button.tsx
+++ frontend/src/a2ui/components/Button/Button.tsx
@@ -7,6 +7,8 @@ export function Button({ node, ctx }: ComponentRendererProps) {
     <button
       type="button"
       className={`a2ui-button a2ui-button--${variant}`}
+      data-a2ui-id={node.id}
+      data-a2ui-component={node.component}
       onClick={() => ctx.dispatchAction(node.action as A2uiAction | undefined, node)}
     >
       {ctx.render(node.child as string)}
