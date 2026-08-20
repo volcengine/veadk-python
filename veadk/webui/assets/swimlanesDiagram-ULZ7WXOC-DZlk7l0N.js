94f50645f91f9b75748cba34ba839544176b96a6
evanlowe <62918515+evanlowe@users.noreply.github.com>
2026-08-20 13:56:13 +0800

    chore(studio): organize built web assets

--- veadk/webui/assets/swimlanesDiagram-ULZ7WXOC-DZlk7l0N.js
+++
@@ -1,8 +0,0 @@
-import{c as r,s as e}from"./flowDiagram-UKHOOZJN-Cq7LzbzP.js";import{a}from"./mermaid.core-rRjQU0ES.js";import"./index-FlDTSoEb.js";import"./chunk-5VM5RSS4-SM4Xpfjc.js";import"./chunk-XXDRQBXY-BDSFeSjL.js";import"./chunk-KBJHAD2P-C6Uy6Ecx.js";import"./chunk-2GRJ4B5K-B4i0qYL8.js";import"./channel-BW273wIk.js";var o=a(t=>`${e(t)}
-  .swimlane.cluster rect {
-    stroke: ${t.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
