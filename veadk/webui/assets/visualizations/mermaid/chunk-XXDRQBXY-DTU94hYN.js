46053b031f1ad97620f8828aa0c369b1250abe1a
xgtcode <102454353+xgtcode@users.noreply.github.com>
2026-08-21 18:12:11 +0800

    fix: fallback system prompt editor parsing failures (#960)
    Co-authored-by: shanchunhua <shanchunhua@bytedance.com>

---
+++ veadk/webui/assets/visualizations/mermaid/chunk-XXDRQBXY-DTU94hYN.js
@@ -0,0 +1,1 @@
+import{a as n}from"./mermaid.core-CL8812F7.js";import{ad as o}from"../../app/index-BPxtKV7r.js";var g=n((t,e)=>{let a;return e==="sandbox"&&(a=o("#i"+t)),(e==="sandbox"?o(a.nodes()[0].contentDocument.body):o("body")).select(`[id="${t}"]`)},"getDiagramElement");export{g};
---
+++ veadk/webui/assets/visualizations/mermaid/swimlanesDiagram-ULZ7WXOC-MeL2gB9c.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN-Ci0q3EoQ.js";import{a}from"./mermaid.core-CL8812F7.js";import"../../app/index-BPxtKV7r.js";import"./chunk-5VM5RSS4-CPG9PzDy.js";import"./chunk-XXDRQBXY-DTU94hYN.js";import"./chunk-KBJHAD2P-ByIA-53A.js";import"./chunk-2GRJ4B5K-ZdSV79QB.js";import"../../chunks/channel-CsmgnEN8.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
