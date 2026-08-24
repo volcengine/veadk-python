0e2921213837d25ff186f7038a48593a2a7ca856
Garming <garming.wu@gmail.com>
2026-08-24 20:38:45 +0800

    build(studio): refresh webui after main rebase

---
+++ veadk/webui/assets/visualizations/mermaid/swimlanesDiagram-ULZ7WXOC-DYbSsggm.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN-BVG7vZMc.js";import{a}from"./mermaid.core-DAY8MwPd.js";import"../../app/index-DrAqnsr8.js";import"./chunk-5VM5RSS4-oQBNLyIn.js";import"./chunk-XXDRQBXY-BFE5_3TL.js";import"./chunk-KBJHAD2P-CHj9xgXx.js";import"./chunk-2GRJ4B5K-DY1GdkS_.js";import"../../chunks/channel-MM3vzSRx.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
