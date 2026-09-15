5ca88e7e4e7e52a9d14a56215d599f128f659a22
zhengchuyi <zhengchuyi@bytedance.com>
2026-09-15 15:03:14 +0800

    fix: bump Studio generated agent veadk default

--- veadk/webui/assets/visualizations/mermaid/chunk-XXDRQBXY-BJ7Goy2I.js
+++
@@ -1,1 +0,0 @@
-import{a as n}from"./mermaid.core-Cf7jiW-Q.js";import{aE as o}from"../../app/index-D-6EdCsW.js";var d=n((t,e)=>{let a;return e==="sandbox"&&(a=o("#i"+t)),(e==="sandbox"?o(a.nodes()[0].contentDocument.body):o("body")).select(`[id="${t}"]`)},"getDiagramElement");export{d as g};
---
+++ veadk/webui/assets/visualizations/mermaid/swimlanesDiagram-ULZ7WXOC-DDky0SOy.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-UKHOOZJN-BA2OxgaA.js";import{a}from"./mermaid.core-qSthvEwX.js";import"../../app/index-BY0KjAh0.js";import"./chunk-5VM5RSS4-IISkJjMJ.js";import"./chunk-XXDRQBXY-BW3FomIE.js";import"./chunk-KBJHAD2P-D_AOsF7_.js";import"./chunk-2GRJ4B5K-tuh4KcR8.js";import"../../chunks/purify.es-BnINGy_Y.js";import"../../chunks/channel-A3uqM-wy.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
