35b91cbb24ebff2857990ffdf0e3b0fd9c0e512f
xgtcode <102454353+xgtcode@users.noreply.github.com>
2026-09-15 17:42:30 +0800

    fix(studio): keep history replies aligned (#1118)
    Co-authored-by: shanchunhua <shanchunhua@bytedance.com>

--- veadk/webui/assets/visualizations/mermaid/chunk-XXDRQBXY-BW3FomIE.js
+++
@@ -1,1 +0,0 @@
-import{a as n}from"./mermaid.core-qSthvEwX.js";import{aE as o}from"../../app/index-BY0KjAh0.js";var d=n((t,e)=>{let a;return e==="sandbox"&&(a=o("#i"+t)),(e==="sandbox"?o(a.nodes()[0].contentDocument.body):o("body")).select(`[id="${t}"]`)},"getDiagramElement");export{d as g};
